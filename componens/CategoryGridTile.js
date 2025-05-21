cddc2b5885a0766c92fb947389a79a4b2f83fbca
MonserratMontor <monserratmontor4@gmail.com>
2025-05-20 18:12:48 -0700

    Se agregaron cambios

--- componens/CategoryGridTile.js
+++ componens/CategoryGridTile.js
@@ -1,34 +1,34 @@
-import { View, Pressable, Text, StyleSheet, Platform} from "react-native";
+import { View, Pressable, Text, StyleSheet, Platform } from "react-native"
 
-function CategoryGridTile(title, color, onPress){
+function CategoryGridTile({title, color, onPress}){
     return (
-        <View style={[StyleSheet.girdItem, {backgroundColor: color}]}>
-            <Pressable android_ripple={{color: '#ccc'}}
-             style={({ pressed }) => [styles.button, pressed ? styles.buttonPressed: null,]}
-                onPress={onPress}>
+        <View style={[styles.gridItem, { backgroundColor: color}]}>
+            <Pressable android_ripple={{color: '#ccc'}} 
+                       style={({ pressed }) => [styles.button, pressed ? styles.buttonPressed : null, ]} 
+                       onPress={onPress}>
                 <View style={styles.innerContainer}>
                     <Text style={styles.title}>
                         {title}
                     </Text>
-                    </View>
-                    </Pressable>
-                    </View>
+                </View>
+            </Pressable>
+        </View>
     )
 }
 
-const styles = StyleSheet.create ({
-    girdItem:{
+const styles = StyleSheet.create({
+    gridItem: {
         flex: 1,
         margin: 16,
-        heinght: 150,
+        height: 150,
         borderRadius: 8,
         elevation: 4,
         backgroundColor: 'white',
         shadowColor: 'black',
         shadowOpacity: 0.25,
-        shadowOffset: {width: 0, height: 2},
+        shadowOffset: { width: 0, height: 2},
         shadowRadius: 8,
-        overflow: Platform.OS == 'android' ? 'hidden' : 'visible'
+        overflow: Platform.OS === 'android' ? 'hidden' : 'visible'
     },
     button: {
         flex: 1
@@ -43,8 +43,8 @@ const styles = StyleSheet.create ({
         justifyContent: 'center',
         alignItems: 'center',
     },
-    tittle: {
-        fontweight: 'bold',
+    title: {
+        fontWeight: 'bold',
         fontSize: 18,
     }
 })
